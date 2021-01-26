1ddde10a01e5dca2399613c983191fca08222d6f
FazalHussain <fazalcs13@gmail.com>
2021-01-26 12:39:57 +0400

    shimmer animation with compose

--- app/src/main/java/com/example/recipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/recipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.recipeapp.presentation.components.*
@@ -51,24 +54,24 @@ class RecipeListFragment : Fragment() {
                             viewModel::onCategoryScrollingStateChanged
                     )
 
-
-
                     //PulsingDemo()
 
                     // Box composable is used to overlap the childrens with each other
                     // The children which is placed at lower would be on top
                     Box(modifier = Modifier.fillMaxSize()) {
-
-                        LazyColumn {
-                            itemsIndexed(items = recipes) { index, recipe ->
-                                RecipeCard(recipe,
-                                        viewModel::changeFavState,
-                                        onClick = { }
-                                )
+                        if (isShowLoading) {
+                            LoadingRecipeShimmer(250.dp)
+                        } else {
+                            LazyColumn {
+                                itemsIndexed(items = recipes) { index, recipe ->
+                                    RecipeCard(recipe,
+                                            viewModel::changeFavState,
+                                            onClick = { }
+                                    )
+                                }
                             }
                         }
-
-                        CircularIndeterminateProgressBar(isDisplayed = isShowLoading)
+                        //CircularIndeterminateProgressBar(isDisplayed = isShowLoading)
                     }
 
 
